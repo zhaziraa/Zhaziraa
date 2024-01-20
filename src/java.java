975366d52d3a4066020542c645f826b88dfc3129
Zhazira <akilova.zhazira9@gmail.com>
2024-01-20 18:54:46 +0600

    version 1

--- src/java.java
+++ src/java.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class java {
-}
+//public class java {
+//    public static void main(String[] args) {
+//        String str = " gal pal mal ";
+//        int n = str.length();
+//        System.out.println(str.length());
+//    }
+//}
